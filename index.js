832d7dcb3e37e3e6204e2981967946470f724fe8
WangXiao0015 <bluewater0015@163.com>
2017-11-07 17:39:37 +0800

    test dev

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ import './css/reset.css';
 import './css/common.css';
 
 import Main from './pages/main';
+import NoFound from './noFound/noFound';
 
 import registerServiceWorker from './registerServiceWorker';
+
+//我需要添加东西
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
